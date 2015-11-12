5862306f9a0ad7c5d28b72cd3c912abd77d5d6ed
Claudia Doppioslash <claudia.doppioslash@gmail.com>
2015-11-12 18:06:45 +0000

    Mostly works

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
 
 var twitter = require('simple-twitter');
 
-twitter = new twitter('x', 'x', 'x', 'x');
+twitter = new twitter( 'x' //consumer key from twitter api
+					 , 'x' //consumer secret key from twitter api
+					 , 'x' //access token from twitter api 
+					 , 'x',
+					 3600); //access token secret from twitter api 
 
-var FeedParser = require('feedparser');
-var request = require('request');
 
-var req = request('https://live-planet-elm.pantheon.io/feeds.xml');
-var feedparser = new FeedParser();
+var http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type' : 'text/plain'});
+	res.end('Planet Elm Bot\n');
+}).listen(8080);
 
-req.on('error', function (error) {
-	console.log(error);
-});
+var timeInterval = 100000; // run every 10m
+var timerVar = setInterval (function () {runBot()}, timeInterval); 
 
-req.on('response', function (res){
-	var stream = this;
+function runBot(){
+	var dateNow = new Date();
+	var FeedParser = require('feedparser');
+	var request = require('request');
 
-	if (res.statusCode != 200 ) return this.emit('error', new Error('Bad status code'));
-	stream.pipe(feedparser);
-});
+	var req = request('https://live-planet-elm.pantheon.io/feeds.xml');
+	var feedparser = new FeedParser();
 
-feedparser.on('error', function(error) {  
-    console.log(error);
-});
+	req.on('error', function (error) {
+		console.log(error);
+	});
 
-feedparser.on('readable', function() {  
-	//TODO: put stuff here
-	var stream = this;
-	var meta = this.meta;
+	req.on('response', function (res){
+		var stream = this;
 
-	var item;
+		if (res.statusCode != 200 ) return this.emit('error', new Error('Bad status code'));
+		stream.pipe(feedparser);
+	});
 
-	while (item = stream.read()) {
-		var titleLength = item.title.length;
-		var itemTitle = item.title;
+	feedparser.on('error', function(error) {  
+	    console.log(error);
+	});
 
-		if (titleLength > 100) {
-			itemTitle = itemTitle.substring(0, 100);
+	feedparser.on('readable', function() {  
+		//TODO: put stuff here
+		var stream = this;
+		var meta = this.meta;
+
+		var item;
+
+		while (item = stream.read()) {
+			var lastRun = dateNow - timeInterval;
+
+			if (item.date > lastRun){
+				var titleLength = item.title.length;
+				var itemTitle = item.title;
+
+				if (titleLength > 100) {
+					itemTitle = itemTitle.substring(0, 100);
+				}
+
+				twitter.post('statuses/update'
+		                    , {'status' : itemTitle + ' ' + item.link + " #elmlang"}
+		                    , function (error, data) {
+		                        console.dir(data);
+		                    });
+			}
 		}
-
-		console.log(itemTitle, item.link, " #elmlang" );
-	}
-})
+	});
+	var dateCompleted = new Date();
+	console.log('loop completed at ' + dateCompleted);
+}
